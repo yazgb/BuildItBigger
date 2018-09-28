24bc004e8cae1c351e1a70c34517abd365568108
YAZMIN GONZALEZ BAYARDO <nimzay@gmail.com>
2018-09-28 00:06:20 -0700

    Add error message to jokeLibrary.

--- jokeLibrary/src/main/java/com/android/yaz/jokelibrary/JokeActivity.java
+++ jokeLibrary/src/main/java/com/android/yaz/jokelibrary/JokeActivity.java
@@ -5,24 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class JokeActivity extends AppCompatActivity {
 
     public static final String JOKE_KEY = "joke_key";
 
+    private TextView mJokeTextView;
+    private TextView mErrorTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_joke);
 
+        mJokeTextView = (TextView) findViewById(R.id.joke_tv);
+        mErrorTextView = (TextView) findViewById(R.id.error_message_tv);
+
         Intent intent = getIntent();
         if(intent.hasExtra(JOKE_KEY)) {
             String joke = intent.getStringExtra(JOKE_KEY);
 
-            TextView jokeTextView = (TextView) findViewById(R.id.joke_tv);
-            if(!TextUtils.isEmpty(joke))
-                jokeTextView.setText(joke);
+            if(!TextUtils.isEmpty(joke)) {
+                showJoke();
+                mJokeTextView.setText(joke);
+            }
+            else
+                showErrorMessage();
         }
+    }
+
+    private void showJoke() {
+        mJokeTextView.setVisibility(View.VISIBLE);
+        mErrorTextView.setVisibility(View.INVISIBLE);
+    }
+
+    private void showErrorMessage() {
+        mJokeTextView.setVisibility(View.INVISIBLE);
+        mErrorTextView.setVisibility(View.VISIBLE);
     }
 }
